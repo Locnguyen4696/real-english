3cf45f02ffecef48afed5ba5651045f99b2f7f91
Loc Nguyen <nguyentongloc1996@gmail.com>
2024-08-24 21:20:08 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  images: {
-    domains: ['therealielts.vn'],
-  }
+ 
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import "../styles/globals.css";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <Header></Header>
-    <Component {...pageProps} />
-    <Footer></Footer>
-    </>
-  );
+    return (
+        <>
+            <Header></Header>
+            <Component {...pageProps} />
+            <Footer></Footer>
+            <GoogleAnalytics gaId="G-BX2C9YEQWP" />
+        </>
+    );
 }
 
 export default MyApp;
